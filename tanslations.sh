9525b9e4e439d79b336256582977cdb697c38a37
leiyue <mr.leiyue@gmail.com>
2016-04-09 17:14:55 +0800

    init repo.

---
+++ tanslations.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+cd ~/.virtualenvs/jks_site/lib/python3.5/site-packages/mezzanine
+cp -r ./accounts/locale/zh_CN ./accounts/locale/zh_hans
+cp -r ./blog/locale/zh_CN ./blog/locale/zh_hans
+cp -r ./conf/locale/zh_CN ./conf/locale/zh_hans
+cp -r ./core/locale/zh_CN ./core/locale/zh_hans
+cp -r ./forms/locale/zh_CN ./forms/locale/zh_hans
+cp -r ./galleries/locale/zh_CN ./galleries/locale/zh_hans
+cp -r ./generic/locale/zh_CN ./generic/locale/zh_hans
+cp -r ./mobile/locale/zh_CN ./mobile/locale/zh_hans
+cp -r ./pages/locale/zh_CN ./pages/locale/zh_hans
+cp -r ./twitter/locale/zh_CN ./twitter/locale/zh_hans
